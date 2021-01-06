fd3d37fc947f19ddbcb1e0d455af0e6559bfe382
philipsinnott <philipsinnotts@gmail.com>
2021-01-06 10:56:03 +0100

    Completed day 1

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,2 +1,12 @@
-print('Hello world!')
+# Create a greeting for your program.
+print("Welcome to @philipsinnott Band Name Generator.")
+# Ask the user for the city that they grew up in.
+city = input("What's the name of the city you grew up in?\n")
+print(city)
+# Ask the user for the name of a pet.
+pet = input("What's your pets name?\n")
+print("Your band name is " + city + " " + pet + "!")
+# Combine the name of their city and pet and show them their band name.
 
+# Make sure the input cursor shows on a new line, see the example at:
+# https://band-name-generator-end.appbrewery.repl.run/
