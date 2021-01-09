67e1e0f3453209ea16892f4c0fba16039fee10af
philipsinnott <philipsinnotts@gmail.com>
2021-01-09 16:50:54 +0100

    Day 3-1 exercise completed

---
+++ day_3/day_3-1.py
@@ -0,0 +1,9 @@
+# Day 3-1
+
+print("Odd or Even? That is the question.")
+number = int(input("Which number do you want to check?\n"))
+
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
